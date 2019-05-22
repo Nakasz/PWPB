9196d47ca9eb9389f5e61f3b14bcec190019f04c
Therapstar <therapstar75@gmail.com>
2019-05-23 05:45:10 +0700

    ini tugas Pwpb

---
+++ important/application/views/_partials/head.php
@@ -0,0 +1,16 @@
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Safario Travel - Home</title>
+	<link rel="icon" href="<?php echo base_url('assets/img/Fevicon.png') ?>" type="image/png">
+
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendors/bootstrap/bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendors/fontawesome/css/all.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendors/themify-icons/themify-icons.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('vendors/linericon/style.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('vendors/owl-carousel/owl.theme.default.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('vendors/owl-carousel/owl.carousel.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('vendors/flat-icon/font/flaticon.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('vendors/nice-select/nice-select.css') ?>">
+
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
---
+++ important/application/views/templates/auth_footer.php
@@ -0,0 +1,14 @@
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="<?= base_url('assets/desainrapstar');?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?= base_url('assets/desainrapstar');?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?= base_url('assets/desainrapstar');?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?= base_url('assets/desainrapstar');?>js/sb-admin-2.min.js"></script>
+  
+</body>
+
+</html>
